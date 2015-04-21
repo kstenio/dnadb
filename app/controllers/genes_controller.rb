a2da3825a8b1da484a0ed9d28ec458a1994d965b
Marcelo Gaioso Werneck <sonata.br@gmail.com>
2015-04-21 18:44:47 -0300

    now working to create new gene

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -11,8 +11,31 @@ class GenesController < ApplicationController
 	end
 
 	def show
-	    @genes = Gene.all
-	    @diseases = Disease.all
+		@gene = Gene.find params[:id]
 	end
+
+	def new
+		@gene = Gene.new
+	end
+
+	def create
+    	@gene = Gene.new gene_params
+
+	    respond_to do |format|
+		    if @gene.save
+		        format.html { redirect_to @gene, notice: 'gene foi criado com sucesso.' }
+		        format.json { render :show, status: :created, location: @gene }
+		    else
+		        format.html { render :new }
+		        format.json { render json: @gene.errors, status: :unprocessable_entity }
+		    end
+	    end
+	end
+
+private
+
+   def gene_params
+      params.require(:gene).permit(:name, :variance, :info)
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :genes
   resources :genes do
 	  collection do
 	    get 'search'
-	    post 'search'
 	  end
 	end
 end
