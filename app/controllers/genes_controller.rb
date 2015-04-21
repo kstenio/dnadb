9c899e6ce905d5208886ed58b884ba5728a52de7
Marcelo Gaioso Werneck <sonata.br@gmail.com>
2015-04-21 12:18:33 -0300

    added search button and funcionality in genes page

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -1,15 +1,18 @@
 class GenesController < ApplicationController
 
-	 def index
-    @genes = Gene.all
-    @diseases = Disease.all
-  end
+	def index
+		if params[:search]
+		    @genes = Gene.search(params[:search]).order("created_at DESC")
+		    @diseases = Disease.all
+		else
+			@diseases = Disease.all
+			@genes = Gene.all
+		end
+	end
 
-  def show
-    @genes = Gene.all
-    @diseases = Disease.all
-    
-  end
-
+	def show
+	    @genes = Gene.all
+	    @diseases = Disease.all
+	end
 
 end
--- app/models/gene.rb
+++ app/models/gene.rb
@@ -1,3 +1,14 @@
 class Gene < ActiveRecord::Base
 	has_many :diseases
+
+	# def self.search(search)
+	# 	search_condition = "%" + search + "%"
+ 	#  		find(:all, :conditions => ['name LIKE ? OR variance LIKE ?', search_condition, search_condition])
+	# end
+
+	def self.search(query)
+	    # where(:title, query) -> This would return an exact match of the query
+	    where("name like ?", "%#{query}%") 
+ 	end
+
 end
