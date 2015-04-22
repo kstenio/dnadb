7a16ddf9ddd46d19b432f933e009a4442186c41e
Marcelo Gaioso Werneck <sonata.br@gmail.com>
2015-04-21 23:47:01 -0300

    modified seeds with some regs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+Gene.create(name: 'abc', variance: 'yyx', info:'Mortal para crianças menores que 12 anos.')
+Gene.create(name: 'xyz', variance: 'x+', info:'Muito raro.')
+Gene.create(name: 'ggf', variance: 'zap', info:'')
+Gene.create(name: 'ab+', variance: 'zyp', info:'Apenas um gene')
+
+Disease.create(name: 'adc', state: 'SP', quantity: 200, gene_id: 1)
+Disease.create(name: 'dex', state: 'SC', quantity: 100, gene_id: 1)
+Disease.create(name: 'yyqr', state: 'BH', quantity: 16524, gene_id: 2)
+
